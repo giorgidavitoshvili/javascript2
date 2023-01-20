524505cdae59d34f647ebbc7178ad2f8bf840bfb
giorgidavitoshvili <119691539+giorgidavitoshvili@users.noreply.github.com>
2023-01-19 16:14:10 -0800

    js homework 2

---
+++ script.js
@@ -0,0 +1,20 @@
+
+person = {
+  name: "GiorGi",
+  age: 18,
+  address: {
+    street: "sarajishvili",
+    number: 40,}
+  }
+console.log (`My name is ${person.name} and I am ${person.age} years old, My address is ${person.address.street} `)
+
+
+if (age < 19 ) {
+  console.log("I am teenager");
+} else if (age > 19 ) {
+  console.log("I am an adult");
+} else { (age != 19)
+  console.log("I am teenager");
+}
+
+console.log(`He was ${results.participant} participant and his score and number is ${results.score} and N${results.number} , he had a score goal ${results.scoregoal} and he had ${results.partners} partners too! `);
